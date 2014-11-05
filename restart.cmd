65aeb2634db3bb282dbf085558cce7e7b8bd28f5
Phobix <devin.p.harris85@gmail.com>
2014-11-04 21:03:26 -0500

    disabled side chat in config and removed noside plugin

--- restart.cmd
+++ restart.cmd
@@ -1,18 +1,16 @@
 @echo off
-echo.
 echo KILL arma2oaserver.exe
 taskkill /im arma2oaserver.exe
-echo.
 ping 127.0.0.1 -n 10 >NUL
-echo.
+echo Pulling from GIT
+cd "c:\program files (x86)\git\bin"
+git --git-dir=E:\DayZ-Epoch\.git pull origin master 
+echo Packing PBO!
+"C:\Program Files\PBO Manager v.1.4 beta\PBOConsole.exe" -pack "E:\DayZ-Epoch\@DayZ_Epoch_Server\addons\dayz_server" "E:\DayZ-Epoch\@DayZ_Epoch_Server\addons\dayz_server.pbo"
 cd "E:\DayZ-Epoch\"
-@start "arma2" /min "E:\DayZ-Epoch\arma2oaserver.exe" "-port=2302" "-config=instance_11_Chernarus\config.cfg" "-cfg=instance_11_Chernarus\basic.cfg" "-profiles=instance_11_Chernarus" -name=instance_11_Chernarus "-mod=@DayZ_Epoch;@DayZ_Epoch_Server;" "-world=Chernarus" "-maxmem=2047" "-noCB"
-echo.
+START "arma2" /min "E:\DayZ-Epoch\arma2oaserver.exe" "-port=2302" "-config=instance_11_Chernarus\config.cfg" "-cfg=instance_11_Chernarus\basic.cfg" "-profiles=instance_11_Chernarus" -name=instance_11_Chernarus "-mod=@DayZ_Epoch;@DayZ_Epoch_Server;" "-world=Chernarus" "-maxmem=2047" "-noCB"
 ping 127.0.0.1 -n 20 >NUL
 echo START BEC
-echo.
-:: start bec
-
 cd "E:\DayZ-Epoch\BEC"
 START "" "E:\DayZ-Epoch\BEC\bec.exe"
 exit
