b7189bd4b1c8e6798a4d95007a93f33011cfb7bb
Jesse Whitworth <jwhitworth@hbs.edu>
2014-11-14 08:20:45 -0500

    Increased price and lowered sell value of medical supplies.

--- MPMissions/DayZ_Epoch_11.Chernarus/custom/custom_loot/CfgServerTrader/Category/FriendlyMedicalSupplies.hpp
+++ MPMissions/DayZ_Epoch_11.Chernarus/custom/custom_loot/CfgServerTrader/Category/FriendlyMedicalSupplies.hpp
@@ -1,37 +1,37 @@
 class Category_541 {
 	class ItemAntibiotic {
 		type = "trade_items";
-		buy[] = {1,"ItemGoldBar"};
-		sell[] = {2,"ItemSilverBar10oz"};
+		buy[] = {5,"ItemGoldBar"};
+		sell[] = {5,"ItemSilverBar10oz"};
 	};
 	class ItemBandage {
 		type = "trade_items";
-		buy[] = {2,"ItemSilverBar"};
-		sell[] = {1,"ItemSilverBar"};
+		buy[] = {2,"ItemSilverBar10oz"};
+		sell[] = {2,"ItemSilverBar"};
 	};
 	class ItemBloodbag {
 		type = "trade_items";
-		buy[] = {2,"ItemSilverBar"};
-		sell[] = {1,"ItemSilverBar"};
+		buy[] = {1,"ItemGoldBar10oz"};
+		sell[] = {1,"ItemGoldBar"};
 	};
 	class ItemEpinephrine {
 		type = "trade_items";
-		buy[] = {2,"ItemSilverBar"};
-		sell[] = {1,"ItemSilverBar"};
+		buy[] = {2,"ItemSilverBar10oz"};
+		sell[] = {2,"ItemSilverBar"};
 	};
 	class ItemHeatPack {
 		type = "trade_items";
-		buy[] = {1,"ItemSilverBar"};
-		sell[] = {1,"ItemSilverBar"};
+		buy[] = {5,"ItemSilverBar10oz"};
+		sell[] = {5,"ItemSilverBar"};
 	};
 	class ItemMorphine {
 		type = "trade_items";
-		buy[] = {2,"ItemSilverBar"};
-		sell[] = {1,"ItemSilverBar"};
+		buy[] = {2,"ItemGoldBar"};
+		sell[] = {2,"ItemSilverBar10oz"};
 	};
 	class ItemPainkiller {
 		type = "trade_items";
-		buy[] = {1,"ItemSilverBar"};
-		sell[] = {1,"ItemSilverBar"};
+		buy[] = {5,"ItemSilverBar10oz"};
+		sell[] = {5,"ItemSilverBar"};
 	};
 };
