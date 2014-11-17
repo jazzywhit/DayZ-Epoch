8f5ccb9e0f15d5ab7057337db00fa3e292f6467d
Jesse Whitworth <jwhitworth@hbs.edu>
2014-11-17 15:03:17 -0500

    Reset ammo prices for shotguns and black market trader.

--- MPMissions/DayZ_Epoch_11.Chernarus/custom/custom_loot/CfgServerTrader/Category/NeutralBlackMarketAmmo.hpp
+++ MPMissions/DayZ_Epoch_11.Chernarus/custom/custom_loot/CfgServerTrader/Category/NeutralBlackMarketAmmo.hpp
@@ -1,72 +1,72 @@
 class Category_527 {
 	class 20Rnd_762x51_SB_SCAR {
 		type = "trade_items";
-		buy[] = {1,"ItemGoldBar"};
-		sell[] = {5,"ItemSilverBar10oz"};
+		buy[] = {4,"ItemGoldBar"};
+		sell[] = {4,"ItemGoldBar"};
 	};
 	class 5Rnd_762x51_M24 {
 		type = "trade_items";
-		buy[] = {1,"ItemSilverBar10oz"};
-		sell[] = {5,"ItemSilverBar"};
+		buy[] = {1,"ItemGoldBar"};
+		sell[] = {1,"ItemGoldBar"};
 	};
     class 20Rnd_762x51_DMR {
         type = "trade_items";
-        buy[] = {2,"ItemSilverBar10oz"};
-        sell[] = {1,"ItemSilverBar10oz"};
+        buy[] = {4,"ItemGoldBar"};
+        sell[] = {4,"ItemGoldBar"};
     };
 	class 30Rnd_556x45_Stanag {
 		type = "trade_items";
-		buy[] = {5,"ItemSilverBar"};
-		sell[] = {3,"ItemSilverBar"};
+		buy[] = {2,"ItemGoldBar"};
+		sell[] = {2,"ItemGoldBar"};
 	};
 	class 20Rnd_762x51_FNFAL {
 		type = "trade_items";
-		buy[] = {4,"ItemSilverBar10oz"};
-		sell[] = {2,"ItemSilverBar10oz"};
+		buy[] = {4,"ItemGoldBar"};
+		sell[] = {4,"ItemGoldBar"};
 	};
 	class 100Rnd_556x45_BetaCMag {
 		type = "trade_items";
-		buy[] = {6,"ItemSilverBar10oz"};
-		sell[] = {3,"ItemSilverBar10oz"};
+		buy[] = {5,"ItemGoldBar"};
+		sell[] = {5,"ItemGoldBar"};
 	};
 	class 30rnd_9x19_MP5 {
 		type = "trade_items";
 		buy[] = {2,"ItemSilverBar10oz"};
-		sell[] = {1,"ItemSilverBar10oz"};
+		sell[] = {2,"ItemSilverBar10oz"};
 	};
-	class 30Rnd_9x19_MP5SD {
+	class 302_9x19_MP5SD {
 		type = "trade_items";
 		buy[] = {4,"ItemSilverBar10oz"};
-		sell[] = {2,"ItemSilverBar10oz"};
+		sell[] = {4,"ItemSilverBar10oz"};
 	};
     class 15Rnd_9x19_M9 {
         type = "trade_items";
-        buy[] = {1,"ItemSilverBar10oz"};
-        sell[] = {5,"ItemSilverBar"};
+        buy[] = {8,"ItemSilverBar"};
+        sell[] = {8,"ItemSilverBar"};
     };
     class 15Rnd_9x19_M9SD {
         type = "trade_items";
         buy[] = {2,"ItemSilverBar10oz"};
-        sell[] = {1,"ItemSilverBar10oz"};
+        sell[] = {2,"ItemSilverBar10oz"};
     };
     class 17Rnd_9x19_glock17 {
         type = "trade_items";
-        buy[] = {1,"ItemSilverBar10oz"};
-        sell[] = {5,"ItemSilverBar"};
+        buy[] = {9,"ItemSilverBar"};
+        sell[] = {9,"ItemSilverBar"};
     };
     class 7Rnd_45ACP_1911 {
         type = "trade_items";
-        buy[] = {2,"ItemSilverBar"};
-        sell[] = {1,"ItemSilverBar"};
+        buy[] = {7,"ItemSilverBar"};
+        sell[] = {7,"ItemSilverBar"};
     };
     class 8Rnd_B_Beneli_74Slug {
         type = "trade_items";
         buy[] = {2,"ItemSilverBar"};
-        sell[] = {1,"ItemSilverBar"};
+        sell[] = {2,"ItemSilverBar"};
     };
     class 8Rnd_B_Beneli_Pellets {
         type = "trade_items";
         buy[] = {2,"ItemSilverBar"};
-        sell[] = {1,"ItemSilverBar"};
+        sell[] = {2,"ItemSilverBar"};
     };
 };
--- MPMissions/DayZ_Epoch_11.Chernarus/custom/custom_loot/CfgServerTrader/Category/NeutralShotgunsandSingleshotAmmo.hpp
+++ MPMissions/DayZ_Epoch_11.Chernarus/custom/custom_loot/CfgServerTrader/Category/NeutralShotgunsandSingleshotAmmo.hpp
@@ -2,21 +2,21 @@ class Category_649 {
 	class 15Rnd_W1866_Slug {
 		type = "trade_items";
 		buy[] = {2,"ItemSilverBar"};
-		sell[] = {1,"ItemSilverBar"};
+		sell[] = {2,"ItemSilverBar"};
 	};
 	class 2Rnd_shotgun_74Pellets {
 		type = "trade_items";
-		buy[] = {2,"ItemSilverBar"};
+		buy[] = {1,"ItemSilverBar"};
 		sell[] = {1,"ItemSilverBar"};
 	};
 	class 2Rnd_shotgun_74Slug {
 		type = "trade_items";
-		buy[] = {2,"ItemSilverBar"};
+		buy[] = {1,"ItemSilverBar"};
 		sell[] = {1,"ItemSilverBar"};
 	};
 	class 10x_303 {
 		type = "trade_items";
-		buy[] = {2,"ItemSilverBar"};
+		buy[] = {1,"ItemSilverBar"};
 		sell[] = {1,"ItemSilverBar"};
 	};
 };
@@ -24,21 +24,21 @@ class Category_613 {
 	class 15Rnd_W1866_Slug {
 		type = "trade_items";
 		buy[] = {2,"ItemSilverBar"};
-		sell[] = {1,"ItemSilverBar"};
+		sell[] = {2,"ItemSilverBar"};
 	};
 	class 2Rnd_shotgun_74Pellets {
 		type = "trade_items";
-		buy[] = {2,"ItemSilverBar"};
+		buy[] = {1,"ItemSilverBar"};
 		sell[] = {1,"ItemSilverBar"};
 	};
 	class 2Rnd_shotgun_74Slug {
 		type = "trade_items";
-		buy[] = {2,"ItemSilverBar"};
+		buy[] = {1,"ItemSilverBar"};
 		sell[] = {1,"ItemSilverBar"};
 	};
 	class 10x_303 {
 		type = "trade_items";
-		buy[] = {2,"ItemSilverBar"};
+		buy[] = {1,"ItemSilverBar"};
 		sell[] = {1,"ItemSilverBar"};
 	};
 };
