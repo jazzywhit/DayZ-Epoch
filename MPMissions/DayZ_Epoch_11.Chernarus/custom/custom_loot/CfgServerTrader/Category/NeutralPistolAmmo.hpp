a5b30a3eefecd4745e8f9aee0f08262a021ddeca
Jesse Whitworth <jwhitworth@hbs.edu>
2014-11-17 14:46:29 -0500

    Reset ammo prices for pistol ammo.

--- MPMissions/DayZ_Epoch_11.Chernarus/custom/custom_loot/CfgServerTrader/Category/NeutralPistolAmmo.hpp
+++ MPMissions/DayZ_Epoch_11.Chernarus/custom/custom_loot/CfgServerTrader/Category/NeutralPistolAmmo.hpp
@@ -1,34 +1,34 @@
 class Category_646 {
 	class 6Rnd_45ACP {
 		type = "trade_items";
-		buy[] = {2,"ItemSilverBar"};
-		sell[] = {1,"ItemSilverBar"};
+		buy[] = {6,"ItemSilverBar"};
+		sell[] = {6,"ItemSilverBar"};
 	};
 	class 8Rnd_9x18_Makarov {
 		type = "trade_items";
-		buy[] = {2,"ItemSilverBar"};
-		sell[] = {1,"ItemSilverBar"};
+		buy[] = {4,"ItemSilverBar"};
+		sell[] = {4,"ItemSilverBar"};
 	};
 	class 8Rnd_9x18_MakarovSD {
 		type = "trade_items";
-		buy[] = {2,"ItemSilverBar10oz"};
-		sell[] = {1,"ItemSilverBar10oz"};
+		buy[] = {8,"ItemSilverBar10oz"};
+		sell[] = {8,"ItemSilverBar10oz"};
 	};
 };
 class Category_611 {
 	class 6Rnd_45ACP {
 		type = "trade_items";
-		buy[] = {2,"ItemSilverBar"};
-		sell[] = {1,"ItemSilverBar"};
+		buy[] = {6,"ItemSilverBar"};
+		sell[] = {6,"ItemSilverBar"};
 	};
 	class 8Rnd_9x18_Makarov {
 		type = "trade_items";
-		buy[] = {2,"ItemSilverBar"};
-		sell[] = {1,"ItemSilverBar"};
+		buy[] = {4,"ItemSilverBar"};
+		sell[] = {4,"ItemSilverBar"};
 	};
 	class 8Rnd_9x18_MakarovSD {
 		type = "trade_items";
-		buy[] = {2,"ItemSilverBar10oz"};
-		sell[] = {1,"ItemSilverBar10oz"};
+		buy[] = {8,"ItemSilverBar10oz"};
+		sell[] = {8,"ItemSilverBar10oz"};
 	};
 };
