7f0bab5bb9ea2e6408312fbc56732e6b666cc6df
Jesse Whitworth <jwhitworth@hbs.edu>
2014-11-23 18:23:16 -0500

    Added shebang line to image copy script.

--- Images/splash.bash
+++ Images/splash.bash
@@ -1,2 +1,3 @@
+#!/bin/sh
 cp loadscreen.jpg ../MPMissions/DayZ_Epoch_11.Chernarus/custom/custom_splash/loadscreen.jpg
 cp deathscreen.jpg ../MPMissions/DayZ_Epoch_11.Chernarus/custom/custom_splash/deathscreen.jpg
