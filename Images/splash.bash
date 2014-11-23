0959c86c2eb36a14baebc414a491ddad0b291ae1
Jesse Whitworth <jwhitworth@hbs.edu>
2014-11-23 11:50:28 -0500

    Combined splash screen folders, updated death screen with smaller jpg file.

--- Images/splash.bash
+++ Images/splash.bash
@@ -1,1 +1,2 @@
 cp loadscreen.jpg ../MPMissions/DayZ_Epoch_11.Chernarus/custom/custom_splash/loadscreen.jpg
+cp deathscreen.jpg ../MPMissions/DayZ_Epoch_11.Chernarus/custom/custom_splash/deathscreen.jpg
