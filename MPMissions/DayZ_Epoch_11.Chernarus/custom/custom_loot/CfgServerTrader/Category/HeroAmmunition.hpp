0c0c8179b27f11dbfce352ba5b4ed4c87cb3d889
MistyDayz <mistdayz@gmail.com>
2014-12-04 08:34:58 +1100

    Fixed extra space possibly causing errors

--- MPMissions/DayZ_Epoch_11.Chernarus/custom/custom_loot/CfgServerTrader/Category/HeroAmmunition.hpp
+++ MPMissions/DayZ_Epoch_11.Chernarus/custom/custom_loot/CfgServerTrader/Category/HeroAmmunition.hpp
@@ -1,8 +1,8 @@
 class Category_478 {
 	class 75Rnd_545x39_RPK {
 		type = "trade_items";
-		buy[] = {3, "itemGoldBar"};
-		sell[] = {1, "itemGoldBar"};
+		buy[] = {3,"ItemGoldBar"};
+		sell[] = {1,"ItemGoldBar"};
 	};
     class 100Rnd_762x54_PK {
         type = "trade_items";
