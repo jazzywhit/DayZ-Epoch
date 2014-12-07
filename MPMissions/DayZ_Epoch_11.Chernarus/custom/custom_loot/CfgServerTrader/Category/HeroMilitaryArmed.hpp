d7614091ba07806d4a404799f8801f257b14c213
Jesse Whitworth <jwhitworth@hbs.edu>
2014-12-07 00:20:02 -0500

    Small pricing updates for the traders.

--- MPMissions/DayZ_Epoch_11.Chernarus/custom/custom_loot/CfgServerTrader/Category/HeroMilitaryArmed.hpp
+++ MPMissions/DayZ_Epoch_11.Chernarus/custom/custom_loot/CfgServerTrader/Category/HeroMilitaryArmed.hpp
@@ -2,27 +2,27 @@ class Category_562 {
 	class HMMWV_M998A2_SOV_DES_EP1_DZE {
 		type = "trade_any_vehicle";
 		buy[] = {8,"ItemBriefcase100oz"};
-		sell[] = {5,"ItemBriefcase100oz"};
+		sell[] = {4,"ItemBriefcase100oz"};
 	};
 	class LandRover_Special_CZ_EP1_DZE {
 		type = "trade_any_vehicle";
 		buy[] = {8,"ItemBriefcase100oz"};
-		sell[] = {5,"ItemBriefcase100oz"};
+		sell[] = {4,"ItemBriefcase100oz"};
 	};
 	class LandRover_MG_TK_EP1_DZE {
 		type = "trade_any_vehicle";
 		buy[] = {4,"ItemBriefcase100oz"};
-		sell[] = {3,"ItemBriefcase100oz"};
+		sell[] = {2,"ItemBriefcase100oz"};
 	};
     class Offroad_DSHKM_Gue_DZE {
         type = "trade_any_vehicle";
-        buy[] = {8,"ItemGoldBar10oz"};
-        sell[] = {4,"ItemGoldBar10oz"};
+        buy[] = {1,"ItemBriefcase100oz"};
+        sell[] = {5,"ItemGoldBar10oz"};
     };
 	class UAZ_MG_TK_EP1_DZE {
 		type = "trade_any_vehicle";
 		buy[] = {2,"ItemBriefcase100oz"};
-		sell[] = {2,"ItemBriefcase100oz"};
+		sell[] = {1,"ItemBriefcase100oz"};
 	};
     class GAZ_Vodnik_MedEvac {
         type = "trade_any_vehicle";
