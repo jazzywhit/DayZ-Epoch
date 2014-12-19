44f2efcb47d60fd91120ef9808c740840baf9797
Jesse Whitworth <jwhitworth@hbs.edu>
2014-12-18 22:14:39 -0500

    Adjusted helicopter prices based on the need to make them more valuable than land vehicles.

--- MPMissions/DayZ_Epoch_11.Chernarus/custom/custom_loot/CfgServerTrader/Category/BanditHelicopterArmed.hpp
+++ MPMissions/DayZ_Epoch_11.Chernarus/custom/custom_loot/CfgServerTrader/Category/BanditHelicopterArmed.hpp
@@ -1,32 +1,32 @@
 class Category_512 {
 	class CH_47F_EP1_DZE {
 		type = "trade_any_vehicle";
-		buy[] = {6,"ItemBriefcase100oz"};
-		sell[] = {4,"ItemBriefcase100oz"};
+		buy[] = {750,"ItemGoldBar"};
+		sell[] = {150,"ItemGoldBar"};
 	};
 	class UH1H_DZE {
 		type = "trade_any_vehicle";
-		buy[] = {3,"ItemBriefcase100oz"};
-		sell[] = {2,"ItemBriefcase100oz"};
+		buy[] = {400,"ItemGoldBar"};
+		sell[] = {80,"ItemGoldBar"};
 	};
 	class Mi17_DZE {
 		type = "trade_any_vehicle";
-		buy[] = {350,"ItemGoldBar"};
-		sell[] = {2,"ItemBriefcase100oz"};
+		buy[] = {400,"ItemGoldBar"};
+		sell[] = {80,"ItemGoldBar"};
 	};
 	class UH60M_EP1_DZE {
 		type = "trade_any_vehicle";
-		buy[] = {4,"ItemBriefcase100oz"};
-		sell[] = {2,"ItemBriefcase100oz"};
+		buy[] = {750,"ItemGoldBar"};
+		sell[] = {150,"ItemGoldBar"};
 	};
 	class UH1Y_DZE {
 		type = "trade_any_vehicle";
-		buy[] = {3,"ItemBriefcase100oz"};
-		sell[] = {1,"ItemBriefcase100oz"};
+		buy[] = {750,"ItemGoldBar"};
+		sell[] = {150,"ItemGoldBar"};
 	};
 	class MH60S_DZE {
 		type = "trade_any_vehicle";
-		buy[] = {4,"ItemBriefcase100oz"};
-		sell[] = {2,"ItemBriefcase100oz"};
+		buy[] = {400,"ItemGoldBar"};
+		sell[] = {80,"ItemGoldBar"};
 	};
 };
--- MPMissions/DayZ_Epoch_11.Chernarus/custom/custom_loot/CfgServerTrader/Category/NeutralHelicopterUnarmed.hpp
+++ MPMissions/DayZ_Epoch_11.Chernarus/custom/custom_loot/CfgServerTrader/Category/NeutralHelicopterUnarmed.hpp
@@ -1,32 +1,32 @@
 class Category_519 {
 	class Mi17_Civilian_DZ {
 		type = "trade_any_vehicle";
-		buy[] = {2,"ItemBriefcase100oz"};
-		sell[] = {1,"ItemBriefcase100oz"};
+		buy[] = {400,"ItemGoldBar"};
+		sell[] = {80,"ItemGoldBar"};
 	};
 	class AH6X_DZ {
 		type = "trade_any_vehicle";
-		buy[] = {9,"ItemGoldBar10oz"};
-		sell[] = {4,"ItemGoldBar10oz"};
+		buy[] = {200,"ItemGoldBar"};
+		sell[] = {40,"ItemGoldBar"};
 	};
 	class MH6J_DZ {
 		type = "trade_any_vehicle";
-		buy[] = {1,"ItemBriefcase100oz"};
-		sell[] = {5,"ItemGoldBar10oz"};
+		buy[] = {300,"ItemGoldBar"};
+		sell[] = {60,"ItemGoldBar"};
 	};
 	class BAF_Merlin_DZE {
 		type = "trade_any_vehicle";
-		buy[] = {2,"ItemBriefcase100oz"};
-		sell[] = {1,"ItemBriefcase100oz"};
+		buy[] = {600,"ItemGoldBar"};
+		sell[] = {120,"ItemGoldBar"};
 	};
 	class CH53_DZE {
 		type = "trade_any_vehicle";
-		buy[] = {2,"ItemBriefcase100oz"};
-		sell[] = {1,"ItemBriefcase100oz"};
+		buy[] = {400,"ItemGoldBar"};
+		sell[] = {80,"ItemGoldBar"};
 	};
 	class Ka137_PMC {
 		type = "trade_any_vehicle";
-		buy[] = {5,"ItemGoldBar10oz"};
-		sell[] = {3,"ItemGoldBar10oz"};
+		buy[] = {50,"ItemGoldBar"};
+		sell[] = {10,"ItemGoldBar"};
 	};
 };
