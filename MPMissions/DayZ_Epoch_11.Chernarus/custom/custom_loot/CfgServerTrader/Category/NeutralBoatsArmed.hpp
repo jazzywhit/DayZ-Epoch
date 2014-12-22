9c3dc130803e5c2a687f0307035c1e21560f77fc
Jesse Whitworth <jwhitworth@hbs.edu>
2014-12-22 13:24:49 -0500

    Added RHIB Turret variant to the boat traders. Increased prices significantly.

--- MPMissions/DayZ_Epoch_11.Chernarus/custom/custom_loot/CfgServerTrader/Category/NeutralBoatsArmed.hpp
+++ MPMissions/DayZ_Epoch_11.Chernarus/custom/custom_loot/CfgServerTrader/Category/NeutralBoatsArmed.hpp
@@ -1,14 +1,24 @@
 class Category_673 {
 	class RHIB {
 		type = "trade_any_boat";
-		buy[] = {7,"ItemGoldBar10oz"};
-		sell[] = {4,"ItemGoldBar10oz"};
+		buy[] = {500,"ItemGoldBar"};
+		sell[] = {100,"ItemGoldBar"};
+	};
+	class RHIB2Turret {
+		type = "trade_any_boat";
+		buy[] = {800,"ItemGoldBar"};
+		sell[] = {160,"ItemGoldBar"};
 	};
 };
 class Category_558 {
 	class RHIB {
 		type = "trade_any_boat";
-		buy[] = {7,"ItemGoldBar10oz"};
-		sell[] = {4,"ItemGoldBar10oz"};
+		buy[] = {500,"ItemGoldBar"};
+		sell[] = {100,"ItemGoldBar"};
+	};
+	class RHIB2Turret {
+		type = "trade_any_boat";
+		buy[] = {800,"ItemGoldBar"};
+		sell[] = {160,"ItemGoldBar"};
 	};
 };
