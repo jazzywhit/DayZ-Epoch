a9e4318a6db5db398b8aeaa42c7d5538bad18581
Jesse Whitworth <jwhitworth@hbs.edu>
2014-12-22 13:25:38 -0500

    Removed turret option.

--- MPMissions/DayZ_Epoch_11.Chernarus/custom/custom_loot/CfgServerTrader/Category/NeutralBoatsArmed.hpp
+++ MPMissions/DayZ_Epoch_11.Chernarus/custom/custom_loot/CfgServerTrader/Category/NeutralBoatsArmed.hpp
@@ -4,21 +4,11 @@ class Category_673 {
 		buy[] = {500,"ItemGoldBar"};
 		sell[] = {100,"ItemGoldBar"};
 	};
-	class RHIB2Turret {
-		type = "trade_any_boat";
-		buy[] = {800,"ItemGoldBar"};
-		sell[] = {160,"ItemGoldBar"};
-	};
 };
 class Category_558 {
 	class RHIB {
 		type = "trade_any_boat";
 		buy[] = {500,"ItemGoldBar"};
 		sell[] = {100,"ItemGoldBar"};
-	};
-	class RHIB2Turret {
-		type = "trade_any_boat";
-		buy[] = {800,"ItemGoldBar"};
-		sell[] = {160,"ItemGoldBar"};
 	};
 };
